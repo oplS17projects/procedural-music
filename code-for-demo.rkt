d01358e447bcb8a402b76c5acae4f23260be7e2e
Samuel Toups <samuel_toups@student.uml.edu>
2017-04-26 11:47:00 -0400

    prepare for demo

--- code-for-demo.rkt
+++ code-for-demo.rkt
@@ -28,18 +28,26 @@
 (define out (make-out-port))
 (open-out-port out "FLUID")
 (open-out-port out "monitor")
-(define test-stream-1 (process-stream 0 (make-note-list) 120))
+(open-out-port out "keyboard")
+(open-out-port out "Turtle")
+;(define test-stream-1 (process-stream 0 (make-note-list) 120))
 ;(define test-stream-2 (process-stream 9 (make-note-list) 120))
 ;(define test-stream-3 (process-stream 11 (make-note-list) 120))
 ;(define test-stream-4 (process-stream 14 (make-note-list) 120))
-(define t1 (play-midi-stream out test-stream-1))
+;(define t1 (play-midi-stream out test-stream-1))
 ;(define t2 (play-midi-stream out test-stream-2))
 ;(define t3 (play-midi-stream out test-stream-3))
 ;(define t4 (play-midi-stream out test-stream-4))
+
+(define chord-primary (make-note-list))
+(define chord-secondary (generate-chord-secondary chord-primary))
+(define primary-thread (play-midi-stream out (process-stream 0 chord-primary 120)))
+(define secondary-thread (play-midi-stream out (process-stream 0 chord-secondary 120)))
+
 
 
 
 (define (generate-chord-secondary notestream)
   (stream-cons
-   (make-key (id-of (stream-first notestream)))
+   (make-note-with-key (make-key (id-of (stream-first notestream))) (duration-of (stream-first notestream)))
    (generate-chord-secondary (stream-rest notestream))))
