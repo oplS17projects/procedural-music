03789a8e4e80ee6dfd8e6b3ce8d70591334a786c
Brian Medina <probune@gmail.com>
2017-04-27 18:21:45 -0400

    Comment for rsound-gen.rkt
    Added some documentation comments to say what stuff does

--- rsound-gen.rkt
+++ rsound-gen.rkt
@@ -6,15 +6,17 @@
 (require portaudio
          ffi/vector)
 
-
+;; math to change midi id into a frequency number
 (define (midi->freq id)
   (* 27.5 (expt 2 (/ (- id 21) 12))))
 
+;; makes a sine wave of the note, plays it, sleeps for the duration, stops after
 (define (rs-play-note note bpm)
   (begin (signal-play (network () (lfo <= square-wave (midi->freq (id-of note)))))
   (sleep (bpm->second bpm (duration-of note)))
   (stop)))
 
+;; iterates through the stream and plays each note until the stream is empty
 (define (rs-play-stream stream)
   (let ((bpm (make-random-bpm)))
     (define (iter stream)
@@ -24,4 +26,5 @@
       (iter (stream-rest stream)))
     (iter stream)))
 
+;; testing stuff
 (rs-play-stream (make-note-list))
